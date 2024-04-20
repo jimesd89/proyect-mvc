068e2ddcb383a0fdf20cb9c656ae5920670f7a6f
jimesd <jime_389@hotmail.com>
2024-04-20 14:54:40 -0300

    primer push

---
+++ BlogMvc/Models/Publicacion.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogMvc.Models
+{
+    public class Publicacion
+    {
+        public string Id { get; set; } = string.Empty;
+        public string SubTitulo { get; set; } = string.Empty;
+        public string Creador { get; set; } = string.Empty;
+        public string Cuerpo { get; set; } = string.Empty;
+        public DateTime Creacion { get; set; } = DateTime.Now;  
+        public string Imagen { get; set; } = string.Empty;
+    }
+}
---
+++ BlogMvc/Rules/PublicacionRule.cs
@@ -0,0 +1,13 @@
+﻿using BlogMvc.Models;
+
+namespace BlogMvc.Rules
+{
+    public class PublicacionRule
+    {
+       public Publicacion GetOnePotsRandom()
+        {
+            return new Publicacion();
+        }
+
+    }
+}
